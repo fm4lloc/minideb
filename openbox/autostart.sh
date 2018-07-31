bf3de2023ac2b83d94828d9b5ca289399aad72e5
Fernando Magalhães <fm4lloc@gmail.com>
2018-07-31 11:15:21 -0300

    Update autostart.sh

--- openbox/autostart.sh
+++ openbox/autostart.sh
@@ -1,7 +1,7 @@
 
 thunar --daemon &
 tint2 &&
-trayer --expand true --transparent true --alpha 255 --edge bottom --align right --expand true --SetDockType true --widthtype request &&
-volumeicon &
+trayer --expand true --transparent true --alpha 255 --edge bottom --align right --expand true --SetDockType true --widthtype request &
 nitrogen --restore &
+volumeicon &
 conky &
